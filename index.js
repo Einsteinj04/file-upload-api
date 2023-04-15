860cc870429dd43750182d9a3469c4df76109b9c
Ukoha Chukwuebuka <jilchrisdascientist@gmail.com>
2023-04-15 14:01:21 +0100

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+// app.use(express.urlencoded({extended:true}))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const { originalname: name, mimetype: type, size } = req.file
+  res.json({name,type,size})
+  next()
 });
 
 
